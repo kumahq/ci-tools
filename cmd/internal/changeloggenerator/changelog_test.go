f60ee694af6045e6e47550ff95850b0bbd53ccb4
Charly Molter <charly.molter@konghq.com>
2023-03-21 10:30:05 +0100

    get rid of ginkgo
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- cmd/internal/changeloggenerator/changelog_test.go
+++ cmd/internal/changeloggenerator/changelog_test.go
@@ -2,29 +2,19 @@ package changeloggenerator_test
 
 import (
 	"github.com/kumahq/ci-tools/cmd/internal/changeloggenerator"
-	. "github.com/onsi/ginkgo"
-	"github.com/onsi/ginkgo/extensions/table"
-	. "github.com/onsi/gomega"
+	"reflect"
 	"testing"
 )
 
-func TestRun(t *testing.T) {
-	RegisterTestingT(t)
-	RunSpecs(t, "changeloggenerator")
-}
-
-var _ = Describe("New", func() {
-	testNewFn := func(in []changeloggenerator.CommitInfo, out changeloggenerator.Changelog) {
-		res, err := changeloggenerator.New("kumahq/kuma", in)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(res).To(HaveLen(len(out)))
-		for i := range out {
-			out[i].Repo = "kumahq/kuma"
-			Expect(res[i]).To(Equal(out[i]))
-		}
+func TestChangelogGenerator(t *testing.T) {
+	type entry struct {
+		desc string
+		in   []changeloggenerator.CommitInfo
+		out  changeloggenerator.Changelog
 	}
-	var _ = table.DescribeTable("dependabot merge", testNewFn,
-		table.Entry("2 simple entries",
+	for _, v := range []entry{
+		{
+			"2 simple entries",
 			[]changeloggenerator.CommitInfo{
 				{PrTitle: "chore(deps): bump foo from 1.2.4 to 1.2.5", PrNumber: 123, Author: "a"},
 				{PrTitle: "chore(deps): bump foo from 1.2.5 to 1.2.7", PrNumber: 124, Author: "a"},
@@ -32,8 +22,9 @@ var _ = Describe("New", func() {
 			changeloggenerator.Changelog{
 				{Desc: "chore(deps): bump foo from 1.2.4 to 1.2.7", Authors: []string{"@a"}, PullRequests: []int{123, 124}},
 			},
-		),
-		table.Entry("with Bump",
+		},
+		{
+			"with bump",
 			[]changeloggenerator.CommitInfo{
 				{PrTitle: "chore(deps): bump foo from 1.2.4 to 1.2.5", PrNumber: 123, Author: "a"},
 				{PrTitle: "chore(deps): Bump foo from 1.2.5 to 1.2.7", PrNumber: 124, Author: "a"},
@@ -41,8 +32,9 @@ var _ = Describe("New", func() {
 			changeloggenerator.Changelog{
 				{Desc: "chore(deps): bump foo from 1.2.4 to 1.2.7", Authors: []string{"@a"}, PullRequests: []int{123, 124}},
 			},
-		),
-		table.Entry("forward then back",
+		},
+		{
+			"forward then rollback",
 			[]changeloggenerator.CommitInfo{
 				{PrTitle: "chore(deps): bump foo from 1.2.4 to 1.2.8", PrNumber: 123, Author: "a"},
 				{PrTitle: "chore(deps): Bump foo from 1.2.8 to 1.2.3", PrNumber: 124, Author: "b"},
@@ -50,8 +42,9 @@ var _ = Describe("New", func() {
 			changeloggenerator.Changelog{
 				{Desc: "chore(deps): bump foo from 1.2.4 to 1.2.3", Authors: []string{"@a", "@b"}, PullRequests: []int{123, 124}},
 			},
-		),
-		table.Entry("not semver",
+		},
+		{
+			"not semver",
 			[]changeloggenerator.CommitInfo{
 				{PrTitle: "chore(deps): bump foo from deadbeef to deadbabe", PrNumber: 123, Author: "a"},
 				{PrTitle: "chore(deps): Bump foo from foew to dead", PrNumber: 124, Author: "b"},
@@ -59,8 +52,9 @@ var _ = Describe("New", func() {
 			changeloggenerator.Changelog{
 				{Desc: "chore(deps): bump foo from deadbeef to dead", Authors: []string{"@a", "@b"}, PullRequests: []int{123, 124}},
 			},
-		),
-		table.Entry("different deps don't get collapsed",
+		},
+		{
+			"different deps don't get collapsed",
 			[]changeloggenerator.CommitInfo{
 				{PrTitle: "chore(deps): bump foo from deadbeef to deadbabe", PrNumber: 123, Author: "a"},
 				{PrTitle: "chore(deps): Bump bar from foew to dead", PrNumber: 124, Author: "b"},
@@ -69,6 +63,22 @@ var _ = Describe("New", func() {
 				{Desc: "chore(deps): bump bar from foew to dead", Authors: []string{"@b"}, PullRequests: []int{124}},
 				{Desc: "chore(deps): bump foo from deadbeef to deadbabe", Authors: []string{"@a"}, PullRequests: []int{123}},
 			},
-		),
-	)
-})
+		},
+	} {
+		t.Run(v.desc, func(t *testing.T) {
+			res, err := changeloggenerator.New("kumahq/kuma", v.in)
+			if err != nil {
+				t.Errorf("%+v", err)
+			}
+			if len(res) != len(v.out) {
+				t.Errorf("not the same length got: %+v expected: %+v", res, v.out)
+			}
+			for i := range v.out {
+				v.out[i].Repo = "kumahq/kuma"
+				if !reflect.DeepEqual(res[i], v.out[i]) {
+					t.Errorf("not the same item at idx %d: %v expected: %v", i, res[i], v.out[i])
+				}
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -1,31 +1,22 @@
 module github.com/kumahq/ci-tools
 
-go 1.18
+go 1.20
 
 require (
 	github.com/google/go-github/v50 v50.0.0
 	github.com/hashicorp/go-multierror v1.1.1
-	github.com/onsi/ginkgo v1.16.5
-	github.com/onsi/gomega v1.27.0
 	github.com/spf13/cobra v1.6.1
 )
 
 require (
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
-	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
-	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
