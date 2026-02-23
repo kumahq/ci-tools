86c109f6e05252e5320fad09519487e7b609b838
Lukasz Dziedziak <lukidzi@gmail.com>
2026-02-23 15:59:07 +0100

    feat(release): added option to extend the release lifetime (#189)
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- cmd/internal/versionfile/version_file_test.go
+++ cmd/internal/versionfile/version_file_test.go
@@ -96,6 +96,37 @@ func TestBuildVersionEntry(t *testing.T) {
 			},
 			versionfile.VersionEntry{Edition: "mesh", Version: "1.2.1", Release: "1.2.x", Latest: true, ReleaseDate: "2020-12-12", EndOfLifeDate: "2021-12-12", Branch: "release-1.2"},
 		),
+		simpleCase(
+			"extended adds months on top of regular lifetime",
+			[]github.GQLRelease{
+				{Name: "1.2.0", Description: "> ExtensionMonths: 6", PublishedAt: d1},
+				{Name: "1.2.1", PublishedAt: d1.Add(time.Hour * 24 * 8), IsLatest: true},
+			},
+			versionfile.VersionEntry{Edition: "mesh", Version: "1.2.1", Release: "1.2.x", Latest: true, ReleaseDate: "2020-12-12", EndOfLifeDate: "2022-06-12", Branch: "release-1.2", ExtensionMonths: 6},
+		),
+		simpleCase(
+			"extended combined with lts adds months on top of lts lifetime",
+			[]github.GQLRelease{
+				{Name: "1.2.0", Description: "> LTS\n> ExtensionMonths: 6", PublishedAt: d1},
+				{Name: "1.2.1", PublishedAt: d1.Add(time.Hour * 48)},
+			},
+			versionfile.VersionEntry{Edition: "mesh", Version: "1.2.1", Release: "1.2.x", LTS: true, ReleaseDate: "2020-12-12", EndOfLifeDate: "2023-06-12", Branch: "release-1.2", ExtensionMonths: 6},
+		),
+		simpleCase(
+			"extended combined with custom release date",
+			[]github.GQLRelease{
+				{Name: "1.2.0", Description: "> Released on 2019/01/01\n> ExtensionMonths: 6"},
+			},
+			versionfile.VersionEntry{Edition: "mesh", Version: "1.2.0", Release: "1.2.x", ReleaseDate: "2019-01-01", EndOfLifeDate: "2020-07-01", Branch: "release-1.2", ExtensionMonths: 6},
+		),
+		simpleCase(
+			"extended on non-first release is ignored",
+			[]github.GQLRelease{
+				{Name: "1.2.1", Description: "> ExtensionMonths: 6", PublishedAt: d1.Add(time.Hour * 48)},
+				{Name: "1.2.0", Description: "foo", PublishedAt: d1},
+			},
+			versionfile.VersionEntry{Edition: "mesh", Version: "1.2.1", Release: "1.2.x", ReleaseDate: "2020-12-12", EndOfLifeDate: "2021-12-12", Branch: "release-1.2"},
+		),
 	} {
 		t.Run(v.desc, func(t *testing.T) {
 			res, err := versionfile.BuildVersionEntry(v.inEdition, v.inReleaseName, v.inLifetimeMonths, v.inLtsLifetimeMonths, v.inReleases)
